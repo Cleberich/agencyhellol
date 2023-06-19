408fbb2cc65103d25605b14ceda58b40a44f0b45
Cleber <cleberrich@192.168.1.3>
2023-06-19 00:06:43 -0300

    commit

--- components/Clientes.js
+++ components/Clientes.js
@@ -3,72 +3,251 @@ import Modal from "./Modal";
 
 const Clientes = () => {
   const [modal, setmodal] = useState(false);
-  const [hover, sethover] = useState(false);
+  const [hover, sethover] = useState(true);
+  const [tooltip, settooltip] = useState(true);
   const efectoHover = () => {
     sethover(true);
+    settooltip(true);
   };
   const efectoSalir = () => {
     sethover(false);
+    settooltip(false);
   };
   return (
     <>
       <div className="grid grid-cols-2 md:grid-cols-4 md:grid-rows-3  bg-[#0E141B] bg-center pt-[450px] md:pt-0">
-        <div id="work" onMouseOver={efectoHover} onMouseOut={efectoSalir}>
-          <img
-            src="clientes/xeptiva.webp"
-            className="hover:scale-[1.01] hover:transition-all ease-in"
-          />
-          {/* <div className=" grid align-middle place-items-center py-2 px-3 rounded-md bg-white relative -mt-[40%] mx-[30%]">
-            <button>See more</button>
-          </div> */}
+        <div className="flip-card ">
+          <div className="flip-card-inner w-full">
+            <div className="flip-card-front">
+              <img src="clientes/xeptiva.webp" className="" />
+            </div>
+            <div className="flip-card-back bg-blue-600 pt-12">
+              <h1>Website </h1>
+              <div>
+                <h2>Technology</h2>
+                <div className="flex justify-center gap-3 mb-3">
+                  <img src="img/next.svg" width={40} />
+                  <img src="img/firebase.png" width={40} />
+                  <img src="img/tail.png" width={40} />
+                </div>
+              </div>
+              <a
+                href="https://www.xeptiva.com/"
+                target="_blank"
+                className="py-2 px-4 w-full hover:bg-pink-700 rounded-md bg-pink-600"
+              >
+                Visit website
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flip-card ">
+          <div className="flip-card-inner w-full">
+            <div className="flip-card-front">
+              <img src="clientes/daniel.webp" className="" />
+            </div>
+            <div className="flip-card-back bg-purple-900 pt-12">
+              <h1>eCommerce</h1>
+              <div>
+                <h2>Technology</h2>
+                <div className="flex justify-center gap-3 mb-3">
+                  <img src="img/next.svg" width={40} />
+                  <img src="img/firebase.png" width={40} />
+                  <img src="img/tail.png" width={40} />
+                </div>
+              </div>
+              <a
+                href="https://ecommerce-dc.vercel.app/"
+                target="_blank"
+                className="py-2 px-4 w-full hover:bg-pink-700 rounded-md bg-pink-600"
+              >
+                Visit website
+              </a>
+            </div>
+          </div>
+          <div className="flip-card-back">
+            <h1>John Doe</h1>
+            <p>Architect & Engineer</p>
+            <p>We love that guy</p>
+          </div>
+        </div>
+        <div className="flip-card ">
+          <div className="flip-card-inner w-full">
+            <div className="flip-card-front">
+              <img src="clientes/ecommerce.webp" className="" />
+            </div>
+            <div className="flip-card-back bg-pink-600 pt-12">
+              <h1>eCommerce</h1>
+              <div>
+                <h2>Technology</h2>
+                <div className="flex justify-center gap-3 mb-3">
+                  <img src="img/react.png" width={50} />
+                  <img src="img/firebase.png" width={40} />
+                  <img src="img/bootstrap.png" width={40} />
+                </div>
+              </div>
+              <a
+                href="https://ecommerce-technology.vercel.app/"
+                target="_blank"
+                className="py-2 px-4 w-full hover:bg-blue-700 rounded-md bg-blue-600"
+              >
+                Visit website
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flip-card ">
+          <div className="flip-card-inner w-full">
+            <div className="flip-card-front">
+              <img src="clientes/shop.webp" className="" />
+            </div>
+            <div className="flip-card-back bg-purple-600 pt-12">
+              <h1>eCommerce</h1>
+              <div>
+                <h2>Technology</h2>
+                <div className="flex justify-center gap-3 mb-3">
+                  <img src="img/js.png" width={40} />
+                  <img src="img/html5.png" width={40} />
+                  <img src="img/bootstrap.png" width={40} />
+                </div>
+              </div>
+              <a
+                href="https://paitoshop.netlify.app/"
+                target="_blank"
+                className="py-2 px-4 w-full hover:bg-red-700 rounded-md bg-red-600"
+              >
+                Visit website
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flip-card ">
+          <div className="flip-card-inner w-full">
+            <div className="flip-card-front">
+              <img src="clientes/paito.webp" className="" />
+            </div>
+            <div className="flip-card-back bg-gray-500 pt-12">
+              <h1>Landing page</h1>
+              <div>
+                <h2>Technology</h2>
+                <div className="flex justify-center gap-3 mb-3">
+                  <img src="img/react.png" width={40} />
+                  <img src="img/styled.png" width={40} />
+                </div>
+              </div>
+              <a
+                href="https://www.paito.uy/"
+                target="_blank"
+                className="py-2 px-4 w-full hover:bg-yellow-700 rounded-md bg-yellow-600"
+              >
+                Visit website
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flip-card ">
+          <div className="flip-card-inner w-full">
+            <div className="flip-card-front">
+              <img src="clientes/mercadolibre.webp" className="" />
+            </div>
+            <div className="flip-card-back bg-yellow-500 pt-12">
+              <h1>App</h1>
+              <div>
+                <h2>Technology</h2>
+                <div className="flex justify-center gap-3 mb-3">
+                  <img src="img/nextjs.png" width={40} />
+                  <img src="img/firebase.png" width={40} />
+                  <img src="img/tail.png" width={40} />
+                </div>
+              </div>
+              <a
+                href="https://mercadolibre-nu.vercel.app/"
+                target="_blank"
+                className="py-2 px-4 w-full hover:bg-blue-700 rounded-md bg-blue-600"
+              >
+                Visit website
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flip-card ">
+          <div className="flip-card-inner w-full">
+            <div className="flip-card-front">
+              <img src="clientes/innovar.webp" className="" />
+            </div>
+            <div className="flip-card-back bg-pink-500 pt-12">
+              <h1>Realstate</h1>
+              <div>
+                <h2>Technology</h2>
+                <div className="flex justify-center gap-3 mb-3">
+                  <img src="img/nextjs.png" width={40} />
+                  <img src="img/firebase.png" width={40} />
+                  <img src="img/node.png" width={40} />
+                  <img src="img/tail.png" width={40} />
+                </div>
+              </div>
+              <a
+                href="https://innovar.vercel.app/index2"
+                target="_blank"
+                className="py-2 px-4 w-full hover:bg-blue-700 rounded-md bg-blue-600"
+              >
+                Visit website
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flip-card ">
+          <div className="flip-card-inner w-full">
+            <div className="flip-card-front">
+              <img src="clientes/actitud.webp" className="" />
+            </div>
+            <div className="flip-card-back bg-orange-500 pt-12">
+              <h1>Landing</h1>
+              <div>
+                <h2>Technology</h2>
+                <div className="flex justify-center gap-3 mb-3">
+                  <img src="img/nextjs.png" width={40} />
+                  <img src="img/firebase.png" width={40} />
+                  <img src="img/node.png" width={40} />
+                  <img src="img/tail.png" width={40} />
+                </div>
+              </div>
+              <a
+                href="https://www.actitudigital.com/"
+                target="_blank"
+                className="py-2 px-4 w-full hover:bg-blue-700 rounded-md bg-blue-600"
+              >
+                Visit website
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="flip-card ">
+          <div className="flip-card-inner w-full">
+            <div className="flip-card-front">
+              <img src="clientes/ciber.webp" className="" />
+            </div>
+            <div className="flip-card-back bg-red-500 pt-12">
+              <h1>Landing</h1>
+              <div>
+                <h2>Technology</h2>
+                <div className="flex justify-center gap-3 mb-3">
+                  <img src="img/wp.png" width={40} />
+                </div>
+              </div>
+              <a
+                href="https://www.ciberoferta.com.uy"
+                target="_blank"
+                className="py-2 px-4 w-full hover:bg-black rounded-md bg-black"
+              >
+                Visit website
+              </a>
+            </div>
+          </div>
         </div>
         <div>
-          <img
-            src="clientes/daniel.webp"
-            className="hover:scale-[1.01] hover:transition-all ease-in"
-          />
-        </div>
-        <div>
-          <img
-            src="clientes/ecommerce.webp"
-            className="hover:scale-[1.01] hover:transition-all ease-in"
-          />
-        </div>
-        <div>
-          <img
-            src="clientes/shop.webp"
-            className="hover:scale-[1.01] hover:transition-all ease-in"
-          />
-        </div>
-        <div>
-          <img
-            src="clientes/paito.webp"
-            className="hover:scale-[1.01] hover:transition-all ease-in"
-          />
-        </div>
-        <div>
-          <img
-            src="clientes/mercadolibre.webp"
-            className="hover:scale-[1.01] hover:transition-all ease-in"
-          />
-        </div>
-        <div>
-          <img
-            src="clientes/innovar.webp"
-            className="hover:scale-[1.01] hover:transition-all ease-in"
-          />
-        </div>
-        <div>
-          <img
-            src="clientes/actitud.webp"
-            className="hover:scale-[1.01] hover:transition-all ease-in"
-          />
-        </div>
-        <div>
-          <img
-            src="clientes/ciber.webp"
-            className="hover:scale-[1.01] hover:transition-all ease-in"
-          />
+          <img src="clientes/ciber.webp" className="invisible" />
         </div>
         {/* <div>
         <img src="clientes/30.gif" />
@@ -95,7 +274,7 @@ const Clientes = () => {
           </svg>
         </h2>
       </div> */}
-        <Modal setmodal={setmodal} />
+        {modal && <Modal modal={modal} setmodal={setmodal} />}
       </div>
     </>
   );
