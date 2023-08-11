31a07184bbc5e57cae20b9b7f14f0f05b986127a
Cleber davila <cleberich@gmail.com>
2023-08-10 21:07:40 -0300

    commit

--- pages/calcular.js
+++ pages/calcular.js
@@ -1,5 +1,6 @@
 import { formatearFecha } from "@/helpers/formatearFecha";
 import Head from "next/head";
+import Link from "next/link";
 import { useState } from "react";
 export default function calcular() {
   const [modal, setmodal] = useState(false);
@@ -60,6 +61,24 @@ export default function calcular() {
             a tu calculadora
           </h1>
         </div>
+        <Link className="fixed top-5 right-5" href="/sueldo">
+          <div className="bg-[#000000] p-3 rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+              />
+            </svg>
+          </div>
+        </Link>
         <div className="w-[360px] mx-auto block md:hidden h-48 md:h-52 bg-[#0B0A0D] shadow-md rounded-3xl">
           <h2 className="font-[500] px-5 pt-5 text-md text-[#626163]">
             {fecha}
