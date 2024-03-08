2c13f397187dc9f56e46a1ae8a7f0e2f745844cd
Cleber davila <cleberich@gmail.com>
2024-03-08 00:24:20 -0300

    commit

--- pages/index5.js
+++ pages/index5.js
@@ -10,7 +10,7 @@ export default function index5() {
         <h2 className="text-[24px] text-[#7a7a7a] mb-4 font-bold text-center">
           Tener el valor correcto de tu propiedad marcara la diferencia entre{" "}
         </h2>
-        <h2 className="text-[46px] font-extrabold text-center -mt-4 text-[#062740]">
+        <h2 className="text-[46px] font-[900] text-center -mt-4 text-[#062740]">
           Vender o NO vender tu inmueble{" "}
         </h2>
         <div className="mx-64">
@@ -46,24 +46,27 @@ export default function index5() {
             </div>
           </div>
         </div>
-        <div className="bg-gradient-to-b from-[#151616] to-[#1F2F3D] py-[115px] px-64">
-          <h3 className="text-[24px] text-white font-bold mb-4">
-            Hablemos de los portales inmobiliarios
-          </h3>
-          <h4 className="text-[20px] text-white font-regular">
-            44.500 apartamentos publicados <br></br>
-            23.800 casas publicadas <br></br>
-            67.800 en total{" "}
-          </h4>
-          <h4 className="text-[20px] text-white font-regular mt-4">
-            de los cuales 1300 se venden por mes esto significa que{" "}
-          </h4>
-          <h4 className="text-[48px] leading-[45px] text-white font-bold mt-4">
-            se vende menos del 2% de lo publicado{" "}
-            <span className="text-[20px] text-white font-regular ">
-              y esto se debe al tener un precio incorrecto
-            </span>
-          </h4>
+        <div className="bg-gradient-to-b from-[#151616] to-[#1F2F3D] py-[115px] sm:px-0 px-12">
+          <main className="max-w-[1290px] flex justify-center flex-col mx-auto">
+            {" "}
+            <h3 className="text-[24px] text-white font-bold mb-4">
+              Hablemos de los portales inmobiliarios
+            </h3>
+            <h4 className="text-[20px] text-white font-regular">
+              44.500 apartamentos publicados <br></br>
+              23.800 casas publicadas <br></br>
+              67.800 en total{" "}
+            </h4>
+            <h4 className="text-[20px] text-white font-regular mt-4">
+              de los cuales 1300 se venden por mes esto significa que{" "}
+            </h4>
+            <h4 className="text-[48px] leading-[45px] text-white font-bold mt-4">
+              se vende menos del 2% de lo publicado{" "}
+              <span className="text-[20px] text-white font-regular ">
+                y esto se debe al tener un precio incorrecto
+              </span>
+            </h4>
+          </main>
         </div>
       </div>
     </>
