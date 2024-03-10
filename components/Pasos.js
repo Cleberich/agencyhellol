8a9fab1015b338f242972d07cc62a5f68c9d34f0
Cleber davila <cleberich@gmail.com>
2024-03-09 23:44:26 -0300

    commit

--- components/Pasos.js
+++ components/Pasos.js
@@ -12,13 +12,7 @@ const Pasos = () => {
         <div className="flex flex-col ">
           {" "}
           <div className="flex justify-center">
-            <img
-              src="/img3/visita.gif"
-              alt="tasacion"
-              className=""
-              width={160}
-              height={160}
-            />
+            <img src="/img3/visita.png" alt="tasacion" className="" />
           </div>
           <h2 className="text-[20px] text-center font-bold mb-4">
             Visita a la propiedad
@@ -30,7 +24,7 @@ const Pasos = () => {
         </div>
         <div className="flex flex-col ">
           <div className="flex justify-center">
-            <img src="/img3/informe.gif" alt="tasacion" className=" mb-5" />
+            <img src="/img3/informe.png" alt="tasacion" className=" mb-5" />
           </div>
           <h2 className="text-[20px] text-start font-bold mb-4">
             Elaboración de informe y tasación
@@ -43,7 +37,7 @@ const Pasos = () => {
         </div>
         <div className="flex flex-col ">
           <div className="flex justify-center">
-            <img src="/img3/entrega.gif" alt="tasacion" className="" />
+            <img src="/img3/entrega.png" alt="tasacion" className="" />
           </div>
           <h2 className="text-[20px] text-center font-bold mb-4">
             Entrega de informe
