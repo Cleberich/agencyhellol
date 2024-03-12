adec25bb456a1db42085aea1df7ee3e68165cc9b
Cleber davila <cleberich@gmail.com>
2024-03-12 13:26:44 -0300

    commit

--- components/Video.js
+++ components/Video.js
@@ -3,15 +3,25 @@ import React from "react";
 const Video = () => {
   return (
     <>
-      {/* <div className="w-[90%] md:w-[720px] mx-auto my-12">
+      <div className="w-[90%] md:w-[720px] mx-auto my-12">
         {" "}
-        <iframe
+        {/* <iframe
           className="w-[100%] mx-auto min-h-[200px] md:min-h-[465px] h-full rounded-xl"
-          src="https://www.youtube.com/embed/SnjG4Z39-e0?rel=0&amp;autoplay=1;?rel=0&amp;controls=0"
-          allow=""
+          src="https://www.youtube.com/watch?v=nyEw2jsfFjg"
+          allow="autoplay"
           frameborder="0"
+        ></iframe> */}
+        <iframe
+          width="951"
+          height="535"
+          src="https://www.youtube.com/embed/nyEw2jsfFjg"
+          title="tasacion profesional"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          className="w-[100%] mx-auto min-h-[200px] md:min-h-[465px] h-full rounded-xl"
+          allowfullscreen
         ></iframe>
-      </div> */}
+      </div>
 
       <h2 className="text-[28px] md:text-[46px] font-[900] text-center mt-12 text-[#062740]">
         ¿Por qué necesito una tasación profesional?
